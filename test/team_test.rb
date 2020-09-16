e2be09dcae361c4495f6ac823822a11e9d79f67b
Kate Tester <katemorris@gmail.com>
2020-09-16 07:30:07 -0600

    Removes unnecessary data and tests from team objects.

--- test/team_test.rb
+++ test/team_test.rb
@@ -1,8 +1,6 @@
-require "./test/test_helper"
-require './lib/stat_tracker'
+require './test/test_helper'
 require './lib/team_manager'
 require './lib/team'
-require 'pry';
 require 'mocha/minitest'
 
 class TeamTest < Minitest::Test
@@ -15,31 +13,11 @@ class TeamTest < Minitest::Test
             'Stadium'      => 'Toyota Stadium',
             'link'         => '/api/v1/teams/6'
             }
-    game_path = './data/dummy_game.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/dummy_game_teams.csv'
-
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
-
-    stat_tracker = StatTracker.from_csv(locations)
-    manager = TeamsManager.new(team_path, stat_tracker)
+    manager = mock('TeamsManager')
     @team = Team.new(data, manager)
   end
 
-  def test_it_exists
-    assert_instance_of Team, @team
-  end
-
-  def test_it_can_find_average_goals
-    assert_equal 2.86, @team.average_goals
-  end
-
-  def test_it_can_find_average_goals_by_type
-    assert_equal 2.75, @team.avg_goals_visitor
-    assert_equal 3.00, @team.avg_goals_home
+  def test_it_has_attributes
+    assert_equal 'FC Dallas', @team.team_name
   end
 end
