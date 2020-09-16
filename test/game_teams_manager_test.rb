e631d7095a2b443862d89e250a627a690f5809e0
Cydnee Owens <cks410@yahoo.com>
2020-09-16 16:18:42 -0600

    Remove double quotes and pry

--- test/game_teams_manager_test.rb
+++ test/game_teams_manager_test.rb
@@ -1,8 +1,7 @@
-require "./test/test_helper"
+require './test/test_helper'
 require './lib/stat_tracker'
 require './lib/game_teams_manager'
 require './lib/game_team'
-require 'pry';
 require 'mocha/minitest'
 
 class GameTeamsManagerTest < Minitest::Test
