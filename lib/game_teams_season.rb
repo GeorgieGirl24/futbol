ee90ca3bd70bf6c6bf23a09c5a990213d28ab7b8
Kate Tester <katemorris@gmail.com>
2020-09-16 09:51:02 -0600

    Adds helper class for season statistics.

---
+++ lib/game_teams_season.rb
@@ -0,0 +1,109 @@
+require_relative './averageable'
+class GameTeamsSeason
+  include Averageable
+  def initialize(manager)
+    @manager = manager
+  end
+
+  def find_season_id(game_id)
+    @manager.find_season_id(game_id)
+  end
+
+  def selected_season_game_teams(season_id)
+    @manager.selected_season_game_teams(season_id)
+  end
+
+  def list_teams_in_season(season_id)
+    selected_season_game_teams(season_id).map do |game_team|
+      game_team.team_id
+    end.uniq
+  end
+
+  def list_game_teams_season_team(season_id, team_id)
+    selected_season_game_teams(season_id).select do |game_team|
+      game_team.team_id == team_id
+    end
+  end
+
+  def wins_for_coach(season_id, head_coach)
+    selected_season_game_teams(season_id).count do |game_team|
+      game_team.result == 'WIN' if game_team.head_coach == head_coach
+    end.to_f
+  end
+
+  def games_for_coach(season_id, head_coach)
+    selected_season_game_teams(season_id).count do |game_team|
+      game_team.head_coach == head_coach
+    end
+  end
+
+  def shots_by_team(season_id, team_id)
+    list_game_teams_season_team(season_id, team_id).sum do |game_team|
+      game_team.shots
+    end
+  end
+
+  def goals_by_team(season_id, team_id)
+    list_game_teams_season_team(season_id, team_id).sum do |game_team|
+      game_team.goals
+    end.to_f
+  end
+
+  def tackles_by_team(season_id, team_id)
+    list_game_teams_season_team(season_id, team_id).sum do |game_team|
+      game_team.tackles
+    end
+  end
+
+  def coaches_hash_avg_win_pct(season_id)
+    by_coach_wins = {}
+    selected_season_game_teams(season_id).each do |game_team|
+      head_coach = game_team.head_coach
+      by_coach_wins[head_coach] ||= []
+      by_coach_wins[head_coach] = average(wins_for_coach(season_id, head_coach), games_for_coach(season_id, head_coach), 2)
+    end
+    by_coach_wins
+  end
+
+  def teams_hash_shots_goals(season_id)
+    by_team_goals_ratio = {}
+    list_teams_in_season(season_id).each do |team_id|
+      by_team_goals_ratio[team_id] ||= []
+      by_team_goals_ratio[team_id] = average(goals_by_team(season_id, team_id), shots_by_team(season_id, team_id))
+    end
+    by_team_goals_ratio
+  end
+
+  def teams_hash_w_tackles(season_id)
+    tackles_by_team = {}
+    list_teams_in_season(season_id).each do |team_id|
+      tackles_by_team[team_id] ||= []
+      tackles_by_team[team_id] = tackles_by_team(season_id, team_id)
+    end
+    tackles_by_team
+  end
+
+  def winningest_coach(season_id)
+    coaches_hash_avg_win_pct(season_id).max_by { |coach, avg_win| avg_win }.to_a[0]
+  end
+
+  def worst_coach(season_id)
+    coaches_hash_avg_win_pct(season_id).min_by { |coach, avg_win| avg_win }.to_a[0]
+  end
+
+  def most_accurate_team(season_id)
+    teams_hash_shots_goals(season_id).max_by { |team, goals_ratio| goals_ratio }.to_a[0]
+  end
+
+  def least_accurate_team(season_id)
+    teams_hash_shots_goals(season_id).min_by { |team, goals_ratio| goals_ratio }.to_a[0]
+  end
+
+  def most_tackles(season_id)
+    teams_hash_w_tackles(season_id).max_by { |team, tackles| tackles }.to_a[0]
+  end
+
+  def fewest_tackles(season_id)
+    teams_hash_w_tackles(season_id).min_by { |team, tackles| tackles }.to_a[0]
+  end
+end
---
+++ test/game_teams_season_test.rb
@@ -0,0 +1,127 @@
+require './test/test_helper'
+require './lib/game_teams_season'
+require './lib/game_teams_manager'
+require './lib/stat_tracker'
+require 'pry';
+require 'mocha/minitest'
+
+class GameTeamsSeasonTest < Minitest::Test
+  def setup
+    game_path = './data/dummy_game.csv'
+    team_path = './data/dummy_teams.csv'
+    game_teams_path = './data/dummy_game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    stat_tracker = StatTracker.from_csv(locations)
+    manager = GameTeamsManager.new(game_teams_path, stat_tracker)
+    @game_team_season = GameTeamsSeason.new(manager)
+  end
+
+  def test_it_can_find_season_id
+    game_id = '2012030221'
+    assert_equal '20122013', @game_team_season.find_season_id(game_id)
+  end
+
+  def test_selected_season_game_teams
+    season_id = '20152016'
+    assert_equal 2, @game_team_season.selected_season_game_teams(season_id).count
+  end
+
+  def test_it_can_list_teams_in_season
+
+  end
+
+  def test_it_can_list_game_teams_season_team
+
+  end
+
+  def test_wins_for_coach
+    season_id = '20122013'
+    head_coach = 'Claude Julien'
+    assert_equal 6.0, @game_team_season.wins_for_coach(season_id, head_coach)
+  end
+
+  def test_games_for_coach
+    season_id = '20152016'
+    head_coach = 'Mike Sullivan'
+    assert_equal 1, @game_team_season.games_for_coach(season_id, head_coach)
+  end
+
+  def test_shots_by_team
+    season_id = '20122013'
+    team_num = '3'
+    assert_equal 38, @game_team_season.shots_by_team(season_id, team_num)
+  end
+
+  def test_goals_by_team
+    season_id = '20122013'
+    team_num = '3'
+    assert_equal 8, @game_team_season.goals_by_team(season_id, team_num)
+  end
+
+  def test_tackles_by_team
+    season_id = '20122013'
+    team_num = '3'
+    assert_equal 179, @game_team_season.tackles_by_team(season_id, team_num)
+  end
+
+  def test_coaches_hash_w_avg_win_percentage
+    season_id = '20152016'
+    expected = {'Mike Sullivan' => 1.0, 'Alain Vigneault' => 0.0}
+    assert_equal expected, @game_team_season.coaches_hash_avg_win_pct(season_id)
+  end
+
+  def test_teams_hash_w_ratio_shots_goals
+    season_id = '20122013'
+    expected = {
+      '3' => 0.2105,
+      '6' => 0.3333,
+      '5' => 0.0769
+    }
+    assert_equal expected, @game_team_season.teams_hash_shots_goals(season_id)
+  end
+
+  def test_teams_hash_w_tackles
+    season_id = '20122013'
+    expected = {
+                  '3' => 179,
+                  '6' => 212,
+                  '5' => 71
+    }
+    assert_equal expected, @game_team_season.teams_hash_w_tackles(season_id)
+  end
+
+  def test_it_can_find_winningest_coach
+    season_id = '20152016'
+    assert_equal 'Mike Sullivan', @game_team_season.winningest_coach(season_id)
+  end
+
+  def test_it_can_find_worst_coach
+    season_id = '20122013'
+    assert_equal 'John Tortorella', @game_team_season.worst_coach(season_id)
+  end
+
+  def test_most_accurate_team
+    season_id = '20122013'
+    assert_equal '6', @game_team_season.most_accurate_team(season_id)
+  end
+
+  def test_least_accurate_team
+    season_id = '20122013'
+    assert_equal '5', @game_team_season.least_accurate_team(season_id)
+  end
+
+  def test_most_tackles
+    season_id = '20122013'
+    assert_equal '6', @game_team_season.most_tackles(season_id)
+  end
+
+  def test_fewest_tackles
+    season_id = '20122013'
+    assert_equal '5', @game_team_season.fewest_tackles(season_id)
+  end
+end
